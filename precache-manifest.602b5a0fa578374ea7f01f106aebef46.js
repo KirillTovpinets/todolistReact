1970d4b4f4dc274e4b26a36ce3536c26634a6b69
Kirill Tovpinec <kirilltovpinec@KirillTovpinets-Macbook-Air.local>
2019-01-31 10:47:08 +0300

    Updates

---
+++ precache-manifest.602b5a0fa578374ea7f01f106aebef46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b655586ed582a2e3b129",
+    "url": "/todolistReact/static/js/runtime~main.b655586e.js"
+  },
+  {
+    "revision": "9ba326d6a15f44743188",
+    "url": "/todolistReact/static/js/main.9ba326d6.chunk.js"
+  },
+  {
+    "revision": "babbbeb6e1f1c6a6b7bd",
+    "url": "/todolistReact/static/js/1.babbbeb6.chunk.js"
+  },
+  {
+    "revision": "9ba326d6a15f44743188",
+    "url": "/todolistReact/static/css/main.007b6412.chunk.css"
+  },
+  {
+    "revision": "babbbeb6e1f1c6a6b7bd",
+    "url": "/todolistReact/static/css/1.c5272a08.chunk.css"
+  },
+  {
+    "revision": "6ad08ca2f9280eb35a79a5eb375f1dd0",
+    "url": "/todolistReact/index.html"
+  }
+];
